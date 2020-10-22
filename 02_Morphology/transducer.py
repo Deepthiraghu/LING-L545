8d5f6f48d4381c264abc4a381ecbc2ca4590f974
Deepthiraghu <deepthiraghu96@gmail.com>
2020-10-22 16:25:03 -0400

    Reorgenized into folders

--- 02_Morphology/transducer.py
+++
@@ -1,6 +0,0 @@
-import hfst
-
-ifs = hfst.HfstInputStream('chv.gen.hfst') # set up an input stream
-transducer = ifs.read()                    # read the first transducer
-transducer.invert()                        # invert the transducer
-transducer.lookup('урамӑн')                # analyse a token
